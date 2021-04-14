1d70d628551f4f358cfff48394dce1a172c95f5d
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-04-13 21:56:48 -0300

    Componentes e countdown

--- store/Countdown/index.ts
+++ store/Countdown/index.ts
@@ -1,32 +1,29 @@
 import { State, Getters, Mutations, MutationsInterface } from './types';
 
-const MINUTES = 25;
+const MINUTES = 0.05;
 
 export const state = (): State => ({
-  time: MINUTES * 60,
-  isActive: false,
-  hasCompleted: false,
+	time: MINUTES * 60,
+	isActive: false,
+	hasCompleted: false,
 });
 
 export const getters: Getters = {
-  minutes: state => Math.floor(state.time / 60),
-  seconds: state => state.time % 60,
+	minutes: state => Math.floor(state.time / 60),
+	seconds: state => state.time % 60,
 };
 
 export const mutations: MutationsInterface = {
-  [Mutations.SET_TIME] (state, newTime) {
-    state.time = newTime;
-  },
-
+	[Mutations.SET_TIME] (state, newTime) {
+		state.time = newTime;
+	},
 	[Mutations.RESET_TIME] (state) {
-    state.time = MINUTES * 60;
-  },
-
-	[Mutations.SET_IS_ACTIVE] (state, isActive)  {
-    state.isActive = isActive;
-  },
-
+		state.time = MINUTES * 60;
+	},
+	[Mutations.SET_IS_ACTIVE] (state, isActive) {
+		state.isActive = isActive;
+	},
 	[Mutations.SET_HAS_COMPLETED] (state, hasCompleted) {
-    state.hasCompleted = hasCompleted;
-  },
+		state.hasCompleted = hasCompleted;
+	},
 };
